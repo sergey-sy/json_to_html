cb884c01f7b4d6705b181505c46b63b77263d439
sergey-sy <maiyashik@gmail.com>
2019-03-27 16:58:12 +0300

    Промежуточная полурабочая версия программы, с направильной последовательностью прохода по структуре данных.

--- project/json_to_html/converter.py
+++ project/json_to_html/converter.py
@@ -2,12 +2,12 @@ import json
 import os
 
 
-class HTMLTagsFactory:
+class HTMLTagsWrapperFactory:
     """
     Class that makes html-tags from text,
     stores it in cache and return outside.
     """
-    __close_tag_cache = None
+    __close_tags_stack = []
 
     def __init__(self):
         self.__tags_cache = {}
@@ -21,27 +21,66 @@ class HTMLTagsFactory:
 
         return self.__tags_cache[open_tag]
 
-    def _parse_json(self, obj):
+    def _parse_json_list(self, obj):
         if isinstance(obj, dict):
             self._parsed_json.append('<li>')
             for key, value in obj.items():
-                open_tag, HTMLTagsFactory.__close_tag = self.get_pair_tags(key)
+                open_close_tags = self.get_pair_tags(key)
+                open_tag = open_close_tags[0]
+                HTMLTagsWrapperFactory.__close_tags_stack.append(
+                                                        open_close_tags[1]
+                                                        )
                 self._parsed_json.append(open_tag)
-                self._parse_json(value)
+                self._parse_json_list(value)
             self._parsed_json.append('</li>')
         elif isinstance(obj, list):
             self._parsed_json.append('<ul>')
             for item in obj:
-                self._parse_json(item)
+                self._parse_json_list(item)
             self._parsed_json.append('</ul>')
         else:
             self._parsed_json.append(obj)
-            self._parsed_json.append(HTMLTagsFactory.__close_tag)
+            try:
+                print(HTMLTagsWrapperFactory.__close_tags_stack)
+                self._parsed_json.append(
+                    HTMLTagsWrapperFactory.__close_tags_stack.pop()
+                    )
+                print('лист', self._parsed_json)
+            except IndexError as err:
+                print(err)
+                print(f'{cls}.__close_tags_stack is empty list.'
+                    'Impossible pop() from empty list'
+                    )
+        print(HTMLTagsWrapperFactory.__close_tags_stack)
 
 
-    def get__parse_json(self, obj):
+    def _parse_json_dict(self, obj):
+        if isinstance(obj, dict):
+            for key, value in obj.items():
+                open_tag, HTMLTagsWrapperFactory.__close_tag = (
+                                                        self.get_pair_tags(key)
+                                                        )
+                self._parsed_json.append(open_tag)
+                self._parse_json_dict(value)
+        elif isinstance(obj, list):
+            for item in obj:
+                self._parse_json_dict(item)
+        else:
+            self._parsed_json.append(obj)
+            self._parsed_json.append(HTMLTagsWrapperFactory.__close_tag)
+
+
+    def get__parse_json_list(self, obj):
         self._parsed_json = []
-        self._parse_json(obj)
+        if isinstance(obj, list):
+            self._parse_json_list(obj)
+        elif isinstance(obj, dict):
+            self._parse_json_dict(obj)
+        else:
+            raise Exception(
+                'Something went wrong, JSON-data contains invalid data format'
+                )
+            
         return self._parsed_json
 
 
@@ -68,8 +107,8 @@ class Converter:
             return json_data
 
     def _convert_json_to_html(self, json_data) -> str:
-        html_factory = HTMLTagsFactory()
-        html_list = html_factory.get__parse_json(json_data)
+        html_factory = HTMLTagsWrapperFactory()
+        html_list = html_factory.get__parse_json_list(json_data)
         html_string = ''.join(html_list)
         return html_string
 
@@ -81,11 +120,18 @@ class Converter:
         except FileNotFoundError as err:
             print(err)
             print(
-                "\nFunction can't find json file with current self._file_path"
-                f"{self}._file_path = {self._file_path}"
+                "\nMethod can't find json file with current self._file_path"
+                f"\n{self}._file_path = {self._file_path}"
                 f"\nUse {self}.set_file_path(file_path) to set correct json "
                  "file path")
         else:
             html_string = self._convert_json_to_html(data)
 
             return html_string
+
+
+if __name__ == '__main__':
+    print('start')
+    conv = Converter('../../json_files/source.json')
+    print(conv.get_file_path())
+    print(conv.convert())
--- project/tests/test_tasks_output.py
+++ project/tests/test_tasks_output.py
@@ -20,8 +20,8 @@ def test_task2_output():
     conv = converter.Converter()
     conv._file_path = 'tests/test_source/source_2.json'
     html_string = conv.convert()
-    sample_tring = '<ul><li><h3>Title #1</h3><div>Hello, World 1!</div></li></ul>'
-    assert html_string == sample_tring
+    sample_string = '<ul><li><h3>Title #1</h3><div>Hello, World 1!</div></li></ul>'
+    assert html_string == sample_string
 
 
 def test_task3_output():
@@ -29,5 +29,23 @@ def test_task3_output():
     conv = converter.Converter()
     conv._file_path = 'tests/test_source/source_3.json'
     html_string = conv.convert()
-    sample_tring = '<ul><li><h3>Title #1</h3><div>Hello, World 1!</div></li><li><h3>Title #2</h3><div>Hello, World 2!</div></li></ul>'
-    assert html_string == sample_tring
+    sample_string = '<ul><li><h3>Title #1</h3><div>Hello, World 1!</div></li><li><h3>Title #2</h3><div>Hello, World 2!</div></li></ul>'
+    assert html_string == sample_string
+
+
+def test_task4_1_output():
+    print('Test-task-4-1')
+    conv = converter.Converter()
+    conv._file_path = 'tests/test_source/source_4_1.json'
+    html_string = conv.convert()
+    sample_string = '<ul><li><span>Title #1</span><content><ul><li><p>Example 1</p><header>header 1</header></li></ul></content></li><li><div>div 1</div></li></ul>'
+    assert html_string == sample_string
+
+
+def test_task4_2_output():
+    print('Test-task-4-2')
+    conv = converter.Converter()
+    conv._file_path = 'tests/test_source/source_4_2.json'
+    html_string = conv.convert()
+    sample_string = '<p>hello1</p>'
+    assert html_string == sample_string
